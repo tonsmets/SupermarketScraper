926e789171dac176479ae681c8cdd11dedfe0514
tonsmets <info@tonsmets.nl>
2015-01-12 21:36:26 +0100

    Added support for Aldi. Not sure why it is not working though...

--- supermarketscraper/supermarkets/runner.py
+++ supermarketscraper/supermarkets/runner.py
@@ -5,6 +5,7 @@ import supermarkets.deka as deka
 import supermarkets.dirk as dirk
 import supermarkets.janlinders as janlinders
 import supermarkets.jumbo as jumbo
+import supermarkets.aldi as aldi
 
 from util.logging import *
 import util.settings as settings
@@ -14,9 +15,10 @@ def test():
 
 def run():
 	ah.fetch()
-	c1000.fetch()
-	coop.fetch()
-	deka.fetch()
-	dirk.fetch()
-	janlinders.fetch()
-	jumbo.fetch()
+	#c1000.fetch()
+	#coop.fetch()
+	#deka.fetch()
+	#dirk.fetch()
+	#janlinders.fetch()
+	#jumbo.fetch()
+	aldi.fetch()
